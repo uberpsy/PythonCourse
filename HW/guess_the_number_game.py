8815ef0b6f8b3c606877f4c2560755dc31370e01
uberpsy <milosh.manov@digitall.com>
2024-02-01 13:22:51 +0200

    guess the number

---
+++ HW/guess_the_number_game.py
@@ -0,0 +1,47 @@
+import random
+
+print( f'{'*'*21} Levels {'*'*21}' )
+print( f'{'* 1. EASY, range: (1, 10), max moves: 7':<49}*' )
+print( f'{'* 2. MEDIUM, range: (1, 10), max moves: 5':<49}*' )
+print( f'{'* 3. HARD, range: (1, 100), max moves: 10':<49}*' )
+print( f'{'*'*50}' )
+
+while True:
+    level = int(input('Select level: '))
+
+    match level:
+        case 1:
+            print("*** Let's play EASY. Good Luck! ***")
+            rand_num = random.randint(1, 10)
+            sel_range = 10
+            tries = 7
+            break
+        case 2:
+            print("*** Let's play Medium. Good Luck! ***")
+            rand_num = random.randint(1, 10)
+            sel_range = 10
+            tries = 5
+            break
+        case 3:
+            print("*** Let's play Hard. Good Luck! ***")
+            rand_num = random.randint(1, 100)
+            sel_range = 100
+            tries = 10
+            break
+        case _:
+            print('Enter a number between 1 and 3')
+
+while tries>0 :
+    user_num = int( input( f'Guess the number (between 1 and {sel_range}): ' ) )
+
+    if user_num > rand_num :
+        tries = tries-1
+        print( f'Too high! Moves left: {tries}' )
+
+    if user_num < rand_num :
+        tries = tries-1
+        print( f'Too low! Moves left: {tries}' )
+        
+    if user_num == rand_num :
+        print( f'*** Congratulations! You guessed it right in {tries} moves!. ***' )
+        break
